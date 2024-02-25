e653a4ddaf4d076bf202465315d0e74efadc5105
Dony Unardi <donyunardi@gmail.com>
2024-02-25 19:11:05 +0000

    delete archive folder

--- code/answer/zzz-archived/01-create-basic-teal-app.R
+++
@@ -1,9 +0,0 @@
-# 1. load the teal package using library() function
-# 2. call the init function and assign it to a variable called `app`
-# 3. call two arguments in the init function, data and modules
-# 4a. for data argument, call the teal_data function and pass two datasets: iris and mtcars
-# 4b. make sure to assign a name to both datasets, IRIS and MTCARS
-# 5. for modules argument, we will be using example_module() function
-# 6. run ls(app) to see the app object
-# 7. pass the app object to the shinyApp() function
-# 8. run the app
